320f7902fcf6c50ba7ce020442c8467bc5df218b
Arkadiusz Cieslak <arekc70@gmail.com>
2019-12-23 14:55:37 +0100

    Updated components to use LanguageContext.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'polish' ? 'Wyślij' : 'Submit';
+
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'polish' ? 'Nazwa:' : 'Name:';
     return (
       <div className="ui field">
-        <label>Name:</label>
+        <label>{text}</label>
         <input/>
       </div>
     );
